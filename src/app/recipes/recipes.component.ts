39106fdf9023135fe78a565a1838715a8e8a44ea
Sam Smith <kso@corcode.com>
2018-11-14 17:30:26 +0100

    14.11

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Name test', 'Desc', 'https://cdn.pixabay.com/photo/2016/02/19/11/53/pug-1210025_960_720.jpg')
+  ];
 
   constructor() { }
 
